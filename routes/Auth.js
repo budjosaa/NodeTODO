8b0a38ee1ed9ef31264930a0013489657de274b5
Dejan <djokicdejan996@gmail.com>
2019-05-31 14:30:24 +0200

    auth1

--- routes/Auth.js
+++ routes/Auth.js
@@ -3,46 +3,47 @@ const router = express.Router();
 const User = require("./models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const env = require("dotenv");
+const dotEnv = env.config();
+getJWT = (user, secret) => {
+  return jwt.sign({ user: user }, secret);
+};
 
 router.post("/register", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then(pass => {
-    const user = new User({
-      username: req.body.username,
-      password: pass,
-      email: req.body.email
-    });
-    user
-      .save()
-      .then(savedUser => {
-        jwt.sign({ user: savedUser }, "secret", (err, token) => {
-          res.json({ message: "You are logged in!", savedUser, token });
-        });
-      })
-      .catch(err => {
-        next(err);
-      });
+  const hashPass = bcrypt.hashSync(req.body.password, 10);
+  const user = new User({
+    username: req.body.username,
+    password: hashPass,
+    email: req.body.email
   });
+  user
+    .save()
+    .then(savedUser => {
+      const token = getJWT(savedUser, process.env.JWT_SECRET);
+      res.json({ savedUser, token });
+    })
+    .catch(err => {
+      next(err);
+    });
 });
+
 router.post("/login", (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
       if (user === null) {
         next(new Error("User not found!"));
       }
-      bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
-        if (err) {
-          res.json({ message: "err" });
-        } else if (isMatch) {
-          jwt.sign({ user: user }, "secret", (err, token) => {
-            res.json({ message: "Logged in", user, token });
-          });
-        } else {
-          res.json({ message: "Wrong password" });
-        }
-      });
+      const compPass = bcrypt.compareSync(req.body.password, user.password);
+      if (compPass) {
+        const token = getJWT(user, process.env.JWT_SECRET);
+        res.json({ user, token });
+      } else {
+        res.json("Wrong password");
+      }
     })
     .catch(err => {
       next(err);
     });
 });
+
 module.exports = router;
